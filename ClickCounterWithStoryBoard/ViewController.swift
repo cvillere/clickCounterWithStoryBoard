f0da25b4a27246a8926ccb829db4aeebf02be28a
Christian Villere <christianvillere@gmail.com>
2017-07-22 13:11:00 -0700

    Project finished using storyboard to complete clickCounter project

--- ClickCounterWithStoryBoard/ViewController.swift
+++ ClickCounterWithStoryBoard/ViewController.swift
@@ -11,14 +11,15 @@ import UIKit
 class ViewController: UIViewController {
     
     var count = 0
-    var label: UILabel!
+    @IBOutlet var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+
     }
     
-    func incrementCount(){
+    @IBAction func incrementCount(){
         self.count += 1
         self.label.text = "\(self.count)"
     }
